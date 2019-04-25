a0c640ed7af8238102f8d7c2c961d87bcc9c94a2
dlevkov <dlevkov@gmail.com>
2019-04-25 14:59:13 +0300

    test fixed

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,21 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  const authMock = { doGoogleLogin: () => {} };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      providers: [{ provide: AuthService, useValue: authMock }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
