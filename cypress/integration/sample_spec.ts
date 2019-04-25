a26b6598591d8f53ebe0662fdb12936712b2c422
dlevkov <dlevkov@gmail.com>
2019-04-25 15:25:10 +0300

    added

--- cypress/integration/sample_spec.ts
+++ cypress/integration/sample_spec.ts
@@ -3,5 +3,17 @@ it('loads examples', () => {
   const url = 'http://localhost:4200';
   cy.visit(url);
   // cy.contains('Kitchen Sink');
-  cy.get('h1').contains('super-heroes-votes');
+  cy.get('img').should('be.visible');
+});
+it('loads login', () => {
+  const url = 'http://localhost:4200/login';
+  cy.visit(url);
+  // cy.contains('Kitchen Sink');
+  cy.get('button').should('be.visible');
+});
+it('loads login', () => {
+  const url = 'http://localhost:4200/select-side';
+  cy.visit(url);
+  // cy.contains('Kitchen Sink');
+  cy.get('p').contains('selects-side works!');
 });
