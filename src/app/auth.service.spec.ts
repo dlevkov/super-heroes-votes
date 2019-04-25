11cce8bb68ac8df93348c8c252befd88ec73af33
dlevkov <dlevkov@gmail.com>
2019-04-25 15:14:51 +0300

    refactor+test fix

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from './auth.service';
-
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
+  let service: AuthService;
+  beforeEach(() => {
+    const angularFireAuthStub = {
+      auth: { signInWithPopup: () => ({ then: () => ({}) }) },
+    };
+    TestBed.configureTestingModule({
+      providers: [AuthService, { provide: AngularFireAuth, useValue: angularFireAuthStub }],
+    });
+    service = TestBed.get(AuthService);
+  });
+  it('can load instance', () => {
     expect(service).toBeTruthy();
   });
 });
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 
 import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase/app';
+import { auth } from 'firebase/app';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private afAuth: AngularFireAuth) {}
   doGoogleLogin() {
-    return new Promise<any>((resolve, reject) => {
-      const provider = new firebase.auth.GoogleAuthProvider();
+    return new Promise<any>(resolve => {
+      const provider = new auth.GoogleAuthProvider();
       provider.addScope('profile');
       provider.addScope('email');
       this.afAuth.auth.signInWithPopup(provider).then(res => {
